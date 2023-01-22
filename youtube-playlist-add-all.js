5e432d4324bf2e6022a0a02d5a8abe6885ca053a
Darren Kameoka <48581401+dkameoka@users.noreply.github.com>
2023-01-21 21:50:21 -1000

    feat: add initial files

---
+++ youtube-playlist-add-all.js
@@ -0,0 +1,103 @@
+async function youtube_wait_for(callback,attempts) {
+    for (let i = 0;i < attempts;i++) {
+        if (callback()) {
+            return true;
+        }
+        await new Promise(resolve => setTimeout(resolve,50));
+    }
+    return false;
+}
+
+async function youtube_playlist_add_all(playlist,limit) {
+    added = 0;
+    videos_buttons = document.querySelectorAll('ytd-browse[role="main"] button[aria-label="Action menu"]');
+    for (let vi = videos_buttons.length - 1;vi >= 0 && added < limit;vi--) {
+        videos_buttons[vi].click();
+
+        // Wait for menu to popup.
+        if (!await youtube_wait_for(function () {
+            menu_popup = document.querySelector('ytd-menu-popup-renderer');
+            return (menu_popup && menu_popup.parentElement.parentElement.style.display == '');
+        },50)) {
+            alert('Failed to open action menu');
+            return;
+        }
+
+        // Click "Save to playlist".
+        menu = document.querySelectorAll('ytd-menu-service-item-renderer');
+        found: {
+            for (let i = 0;i < menu.length;i++) {
+                if (menu[i].innerText != 'Save to playlist') {
+                    continue;
+                }
+                menu[i].click();
+                break found;
+            }
+            // Button not found, so click button again to close it.
+            videos_buttons[vi].click();
+            continue;
+        }
+
+        // Wait for add option menu to popup.
+        if (!await youtube_wait_for(function () {
+            paper_dialog = document.querySelector('tp-yt-paper-dialog');
+            return (paper_dialog && paper_dialog.style.display == '');
+        },50)) {
+            alert('Failed to open add option menu');
+            return;
+        }
+
+        // Click checkbox for playlist
+        add_option_menu = document.querySelectorAll('ytd-playlist-add-to-option-renderer');
+        found: {
+            for (let i = 0;i < add_option_menu.length;i++) {
+                if (add_option_menu[i].innerText != playlist) {
+                    continue;
+                }
+                checkbox = add_option_menu[i].querySelector('#checkbox');
+                if (checkbox == null) {
+                    continue;
+                }
+                if (checkbox.checked) {
+                    break found;
+                }
+                checkbox.click();
+
+                // Wait for check mark.
+                if (!await youtube_wait_for(function () {
+                    return checkbox.checked;
+                },50)) {
+                    alert('Failed to check checkbox');
+                    return;
+                }
+
+                added++;
+                break found;
+            }
+            alert('Could not find playlist "' + playlist + '"');
+            return;
+        }
+
+        // Close the option menu.
+        close = document.querySelector('ytd-add-to-playlist-renderer #close-button button');
+        if (close) {
+            close.click();
+        }
+
+        // Wait for add option menu to close.
+        if (!await youtube_wait_for(function () {
+            paper_dialog = document.querySelector('tp-yt-paper-dialog');
+            return (paper_dialog && paper_dialog.style.display == 'none');
+        },50)) {
+            alert('Failed to open add option menu');
+            return;
+        }
+
+        // Wait for playlist operation.
+        await new Promise(resolve => setTimeout(resolve,500));
+    }
+    console.log('Added ' + added + ' videos to ' + playlist + ' playlist.');
+    alert('Added ' + added + ' videos to ' + playlist + ' playlist');
+}
+
+youtube_playlist_add_all(prompt('Enter name of playlist to add to'),parseInt(prompt('Enter add limit')));
---
+++ youtube-playlist-remove.js
@@ -0,0 +1,83 @@
+function youtube_playlist_is_broken() {
+    if (typeof ytInitialData == 'undefined') {
+        return false;
+    }
+    try {
+        videos = ytInitialData.contents.twoColumnBrowseResultsRenderer.tabs[0].tabRenderer.content.sectionListRenderer.contents[0].itemSectionRenderer.contents[0].playlistVideoListRenderer.contents;
+    } catch (error) {
+        alert('Refresh page to load ytInitialData');
+        return true;
+    }
+    for (let vi = 0;vi < videos.length;vi++) {
+        index = vi + 1;
+        if ('continuationItemRenderer' in videos[vi]) {
+            continue;
+        }
+        video_index = videos[vi].playlistVideoRenderer.index.simpleText || videos[vi].playlistVideoRenderer.index.runs[0].text;
+        if (video_index != index.toString()) {
+            console.log('youtube_playlist_is_broken(): Index ' + video_index + ' is not ' + index +
+                ' for "' + videos[vi].playlistVideoRenderer.title.runs[0].text + '"');
+            return true;
+        }
+    }
+    return false;
+}
+
+async function youtube_wait_for(callback,attempts) {
+    for (let i = 0;i < attempts;i++) {
+        if (callback()) {
+            return true;
+        }
+        await new Promise(resolve => setTimeout(resolve,50));
+    }
+    return false;
+}
+
+async function youtube_playlist_remove(count) {
+    deleted = 0;
+    for (let c = 0;c < count;c++) {
+        // Detect YouTube playlist disappearing videos bug and stop.
+        if (youtube_playlist_is_broken()) {
+            console.log('Detected YouTube playlist bug. Refresh page.');
+            alert('Detected YouTube playlist bug. Refresh page.');
+            return;
+        }
+
+        // Open first video's menu.
+        first_button = document.querySelector('ytd-playlist-video-renderer button');
+        if (first_button == null) {
+            if (deleted == 0) {
+                alert('Empty video list or not on a YouTube /playlist.');
+                return;
+            }
+            break;
+        }
+        first_button.click();
+
+        // Wait for menu to popup.
+        if (!await youtube_wait_for(function () {
+            iron_dropdown = document.querySelector('tp-yt-iron-dropdown[class="style-scope ytd-popup-container"]');
+            return (iron_dropdown && iron_dropdown.style.display == '');
+        },50)) {
+            alert('Failed to open menu');
+            return;
+        }
+
+        // Remove first video.
+        menu = document.querySelectorAll('ytd-menu-popup-renderer ytd-menu-service-item-renderer');
+        for (let i = 0;i < menu.length;i++) {
+            if (menu[i].innerText.startsWith('Remove from ') == false) {
+                continue;
+            }
+            menu[i].click();
+            deleted++;
+        }
+
+        // Wait for playlist to re-render.
+        await new Promise(resolve => setTimeout(resolve,500));
+    }
+    console.log('Removed ' + deleted + ' videos from playlist.');
+    alert('Removed ' + deleted + ' videos from playlist.');
+}
+
+youtube_playlist_remove(parseInt(prompt('Enter number of videos to remove')));
